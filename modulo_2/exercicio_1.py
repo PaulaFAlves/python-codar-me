b6c834e436cc5080e8140287848a0fa5207262a8
paulafalves <paulafernandaalves@icloud.com>
2022-02-08 20:45:48 -0300

    exercicio 2 resolvido

--- modulo_2/exercicio_1.py
+++ modulo_2/exercicio_1.py
@@ -9,3 +9,19 @@ print(type(string_variable))
 print(type(integer_variable))
 print(type(float_variable))
 print(type(bool_variable))
+
+# exercício 2
+
+valor_compras = 12.25
+desconto = 5.5
+quantidade_itens = 2
+
+total_das_compras = valor_compras * quantidade_itens
+total_liquido_das_compras = total_das_compras * (1 - 5.5 / 100)
+
+print(total_das_compras)
+print(total_liquido_das_compras)
+
+custo_medio_por_item = total_liquido_das_compras / quantidade_itens
+
+print(custo_medio_por_item)
