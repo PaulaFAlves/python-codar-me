143e18700dc46a23989c5aa2e72c3aa388a0dbf3
paulafalves <paulafernandaalves@icloud.com>
2022-02-08 20:04:10 -0300

    first exercise

---
+++ modulo_2/exercicio_1.py
@@ -0,0 +1,11 @@
+# exercício 1
+
+string_variable = 'variável de string'
+integer_variable = 12
+float_variable = 12.3
+bool_variable = False
+
+print(type(string_variable))
+print(type(integer_variable))
+print(type(float_variable))
+print(type(bool_variable))
