2d7b2e2421aadd44978245c627ada24a844e808e
paulafalves <paulafernandaalves@icloud.com>
2022-03-09 18:53:34 -0300

    create calculator

---
+++ modulo_13/calculadora/calculadora.py
@@ -0,0 +1,19 @@
+def somar(a, b):
+    return a + b
+
+
+def dividir(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "NAN"
+
+
+def multiplicar(a, b):
+    if a == 0 or b == 0:
+        return 'None'
+    return a * b
+
+
+def subtrair(a, b):
+    return a - b
